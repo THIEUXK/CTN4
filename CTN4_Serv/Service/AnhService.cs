018d964c774850dbb38eb6369ee575e778b4ed5f
THIEUXK <116102779+THIEUXK@users.noreply.github.com>
2023-10-02 20:47:32 +0700

    thieuxk

---
+++ CTN4_Serv/Service/AnhService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CTN4_Data.Models.DB_CTN4;
+using CTN4_Serv.Service.IService;
+
+namespace CTN4_Serv.Service
+{
+    internal class AnhService:IAnhServixe
+    {
+        public List<Anh> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Anh GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Them(Anh a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Sua(Anh a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Xoa(Anh a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
