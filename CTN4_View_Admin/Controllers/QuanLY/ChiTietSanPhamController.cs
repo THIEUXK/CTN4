5fa924b93aefe624dc459d06a280f7c4f0b50174
Phongtxph2301 <117290852+Phongtxph2301@users.noreply.github.com>
2023-10-03 23:16:18 +0700

    á
    SS

---
+++ CTN4_View_Admin/Controllers/QuanLY/ChiTietSanPhamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CTN4_View_Admin.Controllers.QuanLY
+{
+    public class ChiTietSanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
