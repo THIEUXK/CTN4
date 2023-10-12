5a1e8973a3100074ebdb579621f100b0d58508a4
tranbach241203 <117243743+tranbach241203@users.noreply.github.com>
2023-10-12 22:13:15 +0700

    BAck

--- CTN4_Data/DB_Context/DB_CTN4_ok.cs
+++ CTN4_Data/DB_Context/DB_CTN4_ok.cs
@@ -44,7 +44,7 @@ namespace CTN4_Data.DB_Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=MSI;Initial Catalog=CTN4;User ID=sa;Password=thieu12345");
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-Q3TF2SAN\\SQLEXPRESS;Initial Catalog=CTN4;User ID=sa;Password=2412203");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
