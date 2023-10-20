367c4e3af06d070c542c785292b4595765fed760
toanck <toanckph20239@fpt.edu.vn>
2023-10-21 01:52:15 +0700

    toan
    toan

--- CTN4_Serv/ViewModel/HienThiSanPhamView.cs
+++ CTN4_Serv/ViewModel/HienThiSanPhamView.cs
@@ -11,9 +11,20 @@ namespace CTN4_Serv.ViewModel
     public class HienThiSanPhamView
     {
         public List<SanPhamChiTiet> sanPhamChiTiets;
+        public List<Mau> maus;
+        public List<ChatLieu> chatLieus;
         public List<DanhMuc> danhMucs;
         public List<DanhMucChiTiet> danhMucChiTiets;
         public List<DanhMucChiTiet> danhMucChiTiet;
         public List<DanhMucChiTiet> danhMucChiTiet2;
+        public List<SanPhamChiTiet> filterMs;
+        public List<SanPhamChiTiet> filterCl;
+        public PagingInfo pagingInfo { get; set; } = new PagingInfo();
+        public List<SanPhamChiTiet>  sanphampaging {get;set;} = new List<SanPhamChiTiet>();
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int dem;
+        
     }
 }
---
+++ CTN4_Serv/ViewModel/PagingInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTN4_Serv.ViewModel
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+        //public int TotalPages
+        //{
+        //    get
+        //    {
+        //        if (ItemsPerPage <= 0 || TotalItems <= 0)
+        //        {
+        //            return 0; // Hoặc xử lý trường hợp khác tùy theo yêu cầu của bạn
+        //        }
+
+        //        return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+        //    }
+        //}
+    }
+}
