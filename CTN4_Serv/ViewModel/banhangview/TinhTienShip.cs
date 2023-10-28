2cd353cc726a375c2bda1f0f7f35fd592294aac4
THIEUXK <116102779+THIEUXK@users.noreply.github.com>
2023-10-28 09:40:44 +0700

    thieuxk

---
+++ CTN4_Serv/ViewModel/banhangview/TinhTienShip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTN4_Serv.ViewModel.banhangview
+{
+    public class TinhTienShip
+    {
+        
+            //"service_id":53321,
+            //"insurance_value":500000,
+            //"coupon": null,
+            //"from_district_id":1486,
+            //"to_district_id":1493,
+            //"to_ward_code":"20314",
+            //"height": 25,
+            //"length":10,
+            //"weight":3000,
+            //"width": 30
+        public string service_id { get; set; }
+        public string insurance_value { get; set; }
+        public string coupon { get; set; }
+        public string from_district_id { get; set; }
+        public string to_district_id { get; set; }
+        public string to_ward_code { get; set; }
+        public string height { get; set; }
+        public string length { get; set; }
+        public string weight { get; set; }
+        public string width { get; set; }
+    }
+}
