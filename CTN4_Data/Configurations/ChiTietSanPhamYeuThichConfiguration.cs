f1a4a8510bbd69428b59a17d7348491b2238ff43
THIEUXK <116102779+THIEUXK@users.noreply.github.com>
2023-10-31 23:09:23 +0700

    thieuxk

--- CTN4_Data/Configurations/ChiTietSanPhamYeuThichConfiguration.cs
+++ CTN4_Data/Configurations/ChiTietSanPhamYeuThichConfiguration.cs
@@ -9,7 +9,7 @@ namespace CTN4_Data.Configurations
         public void Configure(EntityTypeBuilder<ChiTietSanPhamYeuThich> builder)
         {
             builder.HasKey(c => c.Id);
-            builder.HasOne(c => c.SanPhamChiTiet).WithMany(c => c.CTietSanPhamYeuThiches).HasForeignKey(c => c.IdSanPhamChiTiet);
+            builder.HasOne(c => c.SanPham).WithMany(c => c.CtietSanPhamYeuThiches).HasForeignKey(c => c.IdSanPham);
             builder.HasOne(c => c.SanPhamYeuThich).WithMany(c => c.CTietSanPhamYeuThiches).HasForeignKey(c => c.IdSanPhamYeuThich);
         }
     }
