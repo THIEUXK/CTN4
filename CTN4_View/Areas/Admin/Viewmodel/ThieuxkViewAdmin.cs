e2eb927a1bbc4130c29ef270e47b8096d21fb467
THIEUXK <116102779+THIEUXK@users.noreply.github.com>
2023-11-03 22:54:26 +0700

    thieuxk

--- CTN4_View/Areas/Admin/Viewmodel/ThieuxkViewAdmin.cs
+++ CTN4_View/Areas/Admin/Viewmodel/ThieuxkViewAdmin.cs
@@ -1,11 +1,24 @@
-﻿using CTN4_Data.Models.DB_CTN4;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CTN4_Data.Models.DB_CTN4;
 
-namespace CTN4_View.Areas.Admin.Viewmodel
+namespace CTN4_Serv.ViewModel
 {
-    public class ThieuxkViewAdmin
+    public class ThieuxkView
     {
-        public List<HoaDon> hoaDons { get; set; }
-        public HoaDon HoaDon { get; set; }
+        public List<SanPham> sanPhams { get; set; }
+        public List<SanPhamChiTiet> sanPhamChiTiets { get; set; }
+        public SanPhamChiTiet SanPhamChiTiet { get; set; }
+        public List<Anh> AhList { get; set; }
+        public Guid IdMau { get; set; }
+
+        public SanPham SanPham { get; set; }
         public List<HoaDonChiTiet> hoaDonChiTiets { get; set; }
+
+        public HoaDon HoaDon { get; set; }
+
     }
 }
