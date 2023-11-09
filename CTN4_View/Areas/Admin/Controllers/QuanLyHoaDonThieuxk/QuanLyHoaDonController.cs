8ec156236bb10eb6cf14b34d94246e4849baf48c
THIEUXK <116102779+THIEUXK@users.noreply.github.com>
2023-11-09 17:19:10 +0700

    thieuxk

--- CTN4_View/Areas/Admin/Controllers/QuanLyHoaDonThieuxk/QuanLyHoaDonController.cs
+++ CTN4_View/Areas/Admin/Controllers/QuanLyHoaDonThieuxk/QuanLyHoaDonController.cs
@@ -20,20 +20,129 @@ namespace CTN4_View.Areas.Admin.Controllers.QuanLyHoaDonThieuxk
             var hd = _hoaDonService.GetAll();
             var view = new ThieuxkViewAdmin()
             {
-            hoaDons = hd,
+                hoaDons = hd,
             };
             return View(view);
         }
         public IActionResult XemChiTiet(Guid id)
         {
             var hd = _hoaDonService.GetById(id);
-            var hdct = _hoaDonChiTietService.GetAll().Where(c=>c.IdHoaDon==id).ToList();
+            var hdct = _hoaDonChiTietService.GetAll().Where(c => c.IdHoaDon == id).ToList();
             var view = new ThieuxkViewAdmin()
             {
                 HoaDon = hd,
-                hoaDonChiTiets=hdct
+                hoaDonChiTiets = hdct
             };
             return View(view);
+        }
+        public IActionResult XacNhanDonHang(Guid id)
+        {
+            var hd = _hoaDonService.GetById(id);
+            hd.TrangThai = "Đang chuẩn bị hàng";
+            _hoaDonService.Sua(hd);
+            var hdct = _hoaDonChiTietService.GetAll().Where(c => c.IdHoaDon == id).ToList();
+            var view = new ThieuxkViewAdmin()
+            {
+                HoaDon = hd,
+                hoaDonChiTiets = hdct
+            };
+            return View("XemChiTiet", view);
+        }
+        public IActionResult XacNhanGiaoHang(Guid id)
+        {
+            var hd = _hoaDonService.GetById(id);
+            if (hd.TrangThai == "Đang chuẩn bị hàng")
+            {
+                hd.TrangThai = "Hàng của bạn đang được giao";
+                hd.NgayGiao = DateTime.Now;
+                _hoaDonService.Sua(hd);
+            }
+            else
+            {
+                var message = "Đơn hàng chưa được xác nhận !";
+                TempData["TB1"] = message;
+                return RedirectToAction("XemChiTiet", new { id = id, message });
+            }
+
+            var hdct = _hoaDonChiTietService.GetAll().Where(c => c.IdHoaDon == id).ToList();
+            var view = new ThieuxkViewAdmin()
+            {
+                HoaDon = hd,
+                hoaDonChiTiets = hdct
+            };
+            return View("XemChiTiet", view);
+        }
+        public IActionResult XacNhanThanhToan(Guid id)
+        {
+            var hd = _hoaDonService.GetById(id);
+            if (hd.TrangThai == "Đang chuẩn bị hàng")
+            {
+                hd.TrangThaiThanhToan = true;
+                _hoaDonService.Sua(hd);
+            }
+            else
+            {
+                var message = "Đơn hàng chưa được xác nhận !";
+                TempData["TB3"] = message;
+                return RedirectToAction("XemChiTiet", new { id = id, message });
+            }
+
+
+            var hdct = _hoaDonChiTietService.GetAll().Where(c => c.IdHoaDon == id).ToList();
+            var view = new ThieuxkViewAdmin()
+            {
+                HoaDon = hd,
+                hoaDonChiTiets = hdct
+            };
+            return View("XemChiTiet", view);
+        }
+        public IActionResult XacNhanNhanHang(Guid id)
+        {
+            var hd = _hoaDonService.GetById(id);
+            if (hd.TrangThai == "Hàng của bạn đang được giao" && hd.TrangThaiThanhToan == true)
+            {
+                hd.TrangThai = "Giao hàng thành công";
+                hd.NgayNhan = DateTime.Now;
+                _hoaDonService.Sua(hd);
+            }
+            else
+            {
+                var message = "Đơn hàng phải được giao vào thanh toán !";
+                TempData["TB2"] = message;
+                return RedirectToAction("XemChiTiet", new { id = id, message });
+            }
+
+            var hdct = _hoaDonChiTietService.GetAll().Where(c => c.IdHoaDon == id).ToList();
+            var view = new ThieuxkViewAdmin()
+            {
+                HoaDon = hd,
+                hoaDonChiTiets = hdct
+            };
+            return View("XemChiTiet", view);
+        }
+        public IActionResult HuyDon(Guid id)
+        {
+            var hd = _hoaDonService.GetById(id);
+            if (hd.NgayGiao == null)
+            {
+                hd.Is_detele = false;
+                hd.TrangThai = "đơn hàng đã bị hủy";
+                _hoaDonService.Sua(hd);
+            }
+            else
+            {
+                var message = "Đơn hàng không thể hủy vì đang được giao";
+                TempData["TB4"] = message;
+                return RedirectToAction("XemChiTiet", new { id = id, message });
+            }
+
+            var hdct = _hoaDonChiTietService.GetAll().Where(c => c.IdHoaDon == id).ToList();
+            var view = new ThieuxkViewAdmin()
+            {
+                HoaDon = hd,
+                hoaDonChiTiets = hdct
+            };
+            return View("XemChiTiet", view);
         }
     }
 }
