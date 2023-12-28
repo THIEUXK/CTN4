56b69ba8cf8f78de2232362194c33943d574d766
tranbach241203 <117243743+tranbach241203@users.noreply.github.com>
2023-12-28 15:40:04 +0700

    bách

---
+++ CTN4_View/Areas/Admin/Viewmodel/EditVoucherViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CTN4_Data.Models.DB_CTN4;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTN4_View.Areas.Admin.Viewmodel
+{
+    public class EditGiamGiaViewModel
+    {
+        public GiamGia GiamGia { get; set; }
+        public bool IsReadOnly { get; set; }
+    }
+
+    // Trong Controller
+
+}
