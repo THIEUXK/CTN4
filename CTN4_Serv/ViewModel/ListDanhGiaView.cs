d3d95c227aca29fb2badca277b652b56df2c8cd8
toanck <toanckph20239@fpt.edu.vn>
2024-01-13 12:10:36 +0700

    toan

---
+++ CTN4_Serv/ViewModel/ListDanhGiaView.cs
@@ -0,0 +1,17 @@
+﻿using CTN4_Data.Models;
+using CTN4_Data.Models.DB_CTN4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTN4_Serv.ViewModel
+{
+    public class ListDanhGiaView
+    {
+         public List<DanhGiaSanPham> SanPhamDaDanhGia { get; set; }
+         public List<DanhGiaSanPham> DuyetDanhGia { get; set; }
+         public List<DanhGiaSanPham> DanhGiaBiAn { get; set; }
+    }
+}
